12ebfd1096961dfba2dce5b7d873429dbdb366c0
AndrewGarven <15ajg6@queensu.ca>
2026-02-20 14:29:13 -0500

    allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -22,8 +22,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]  # tighten later
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
